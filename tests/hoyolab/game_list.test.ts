e101ef1ba2d86ebecd4216e814f7e41586a8c959
Linh Nguyễn <vanlinh14121@gmail.com>
2024-06-20 15:00:55 +0700

    update config

---
+++ tests/hoyolab/game_list.test.ts
@@ -0,0 +1,14 @@
+import test from 'ava'
+import { hoyolab } from './setup'
+
+test('get game list', async (t) => {
+  const client = await hoyolab()
+  const res = await client.gamesList()
+
+  if (res) {
+    console.log(res)
+    t.pass()
+  } else {
+    t.fail()
+  }
+})
