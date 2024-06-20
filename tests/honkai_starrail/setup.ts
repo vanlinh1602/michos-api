c3b06c2d1eee84703c50a57eb10fa87d6cc0572b
Linh Nguyễn <vanlinh14121@gmail.com>
2024-06-20 17:02:05 +0700

    update code

--- tests/honkai_starrail/setup.ts
+++ tests/honkai_starrail/setup.ts
@@ -12,10 +12,11 @@ export const cookie: ICookie = {
   cookieTokenV2: process.env.COOKIE_TOKEN_V2 ?? '',
   accountIdV2: parseInt(process.env.ACCOUNT_ID_V2 ?? '0'),
   accountMidV2: process.env.ACCOUNT_MID_V2,
+  ltmidV2: process.env.LTMID_V2 ?? '',
 }
 
-export const hsr = async () => {
-  return await HonkaiStarRail.create({
+export const hsr = () => {
+  return new HonkaiStarRail({
     cookie,
     lang: LanguageEnum.VIETNAMESE,
     uid: parseInt(process.env.HSR_UID ?? '0'),
