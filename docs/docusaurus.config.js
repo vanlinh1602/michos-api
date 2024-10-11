78fb1d0101a65d1bc27f345b79aad75f6ffd1a0a
Nguyễn Văn Linh <linhnv1622@gmail.com>
2024-10-11 14:15:30 +0700

    update config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon-32x32.png',
 
   // Set the production url of your site here
-  url: 'https://monsieur-kuma.github.io',
+  url: 'https://vanlinh1602.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/michos-api/',
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'monsieur-kuma', // Usually your GitHub org/user name.
+  organizationName: 'vanlinh1602', // Usually your GitHub org/user name.
   projectName: 'michos-api', // Usually your repo name.
 
   onBrokenLinks: 'warn',
@@ -47,7 +47,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/monsieur-kuma/michos-api/tree/master/docs/',
+            'https://github.com/vanlinh1602/michos-api/tree/master/docs/',
         },
         blog: false,
         theme: {
@@ -76,7 +76,7 @@ const config = {
             position: 'left',
           },
           {
-            href: 'https://github.com/monsieur-kuma/michos-api',
+            href: 'https://github.com/vanlinh1602/michos-api',
             label: 'GitHub',
             position: 'right',
           },
